68e6e86f902b60ed1ee0f912a2e45271d4c53c48
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2019-05-13 18:25:42 +0530

    controller methods and views

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,5 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -8,9 +10,12 @@ namespace MvcMovie.Models
         public int Id {get; set;}
         public string Title {get; set;}
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set;}
         public string Genre {get; set;}
+        
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price {get; set;}
     }
 }
