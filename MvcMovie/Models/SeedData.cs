46cb2ce6afbc1aabfe3e37a0d88faec8c23621b7
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2019-05-13 15:20:28 +0530

    add seeding

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext (
+                serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()
+            ))
+            {
+                // look for any movies.
+                if (!context.Movie.Any())
+                {
+                    context.Movie.AddRange(
+                        new Movie
+                        {
+                            Title = "When Harry Met Sally",
+                            ReleaseDate = DateTime.Parse("1989-2-12"),
+                            Genre = "Romantic Comedy",
+                            Price = 7.99m
+                        },
+                        new Movie
+                        {
+                            Title = "When Harry Met Sally",
+                            ReleaseDate = DateTime.Parse("1989-2-12"),
+                            Genre = "Romantic Comedy",
+                            Price = 7.99m
+                        },
+                        new Movie
+                        {
+                            Title = "When Harry Met Sally",
+                            ReleaseDate = DateTime.Parse("1989-2-12"),
+                            Genre = "Romantic Comedy",
+                            Price = 7.99m
+                        },
+                        new Movie
+                        {
+                            Title = "When Harry Met Sally",
+                            ReleaseDate = DateTime.Parse("1989-2-12"),
+                            Genre = "Romantic Comedy",
+                            Price = 7.99m
+                        }
+                    );
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
